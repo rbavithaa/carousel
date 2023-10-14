dd444b01f95f7d084566427ee2ce89f46a30bafb
rbavithaa <147941327+rbavithaa@users.noreply.github.com>
2023-10-14 23:25:44 +0530

    Delete carousel.js

--- carousel.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.experts-talk-slider').slick({
-        slidesToShow: 3, // Number of slides to show at once
-        slidesToScroll: 1, // Number of slides to scroll at a time
-        autoplay: true, // Autoplay the carousel
-        autoplaySpeed: 2000, // Autoplay speed in milliseconds
-        dots: true, // Show navigation dots
-    });
-});
